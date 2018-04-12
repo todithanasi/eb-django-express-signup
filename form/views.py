a4b44c3c60d71073dae2ec957d0d461acd7aafb3
todi.thanasi <todi.thanasi@gmail.com>
2018-04-12 17:01:13 +0200

    Part 1
    Part 1

--- form/views.py
+++ form/views.py
@@ -9,6 +9,7 @@ from django.conf import settings
 import json
 from django.http import JsonResponse
 from django.core import serializers
+from django.http import JsonResponse
 
 BASE_DIR = getattr(settings, "BASE_DIR", None)
 
@@ -38,21 +39,6 @@ def search(request):
         domain_count.update([item['email'].split('@')[1] for item in items])
         return render(request, 'search.html', {'domains': sorted(domain_count.items())})
 
-# def chart(request):
-#     domain = request.GET.get('domain')
-#     preview = request.GET.get('preview')
-#     leads = Leads()
-#     items = leads.get_leads(domain, preview)
-#     domain_count = Counter()
-#     domain_count.update([item['email'].split('@')[1] for item in items])
-#     domain_freq = domain_count.most_common(15)
-#     if len(domain_freq) == 0:
-#         return HttpResponse('No items to show', status=200)
-#     labels, freq = zip(*domain_freq)
-#     data = {'data': freq, 'x': labels}
-#     bar = vincent.Bar(data, iter_idx='x')
-#     bar.to_json(os.path.join(BASE_DIR, 'static', 'domain_freq.json'))
-#     return render(request, 'chart.html', {'items': []})
 
 def chart(request):
     domain = request.GET.get('domain')
@@ -67,7 +53,5 @@ def chart(request):
     labels, freq = zip(*domain_freq)
     data = {'data': freq, 'x': labels}
     bar = vincent.Bar(data, iter_idx='x')
-    items_to_show = bar.to_json()
-    # items_to_show = serializers.serialize('json', items_to_show)
-    # return JsonResponse(items_to_show, "DjangoJSONEncoder", False)
-    return render(request, 'chart.html', {"items_to_show": items_to_show},"application/json")
+    items_to_show = json.dumps(bar.to_json())
+    return render(request, 'chart.html', {'items_to_show': items_to_show})
